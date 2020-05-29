636f426fb4f2381c0fa88b19c65bb06b56248cc7
SerdiukNaz2018 <45904119+SerdiukNaz2018@users.noreply.github.com>
2020-05-29 16:08:55 +0300

    Initial Commit

---
+++ Component/Component/Button.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Component
+{
+    public class Button: Control
+    {
+        private StringFormat SF = new StringFormat();
+        private bool MouseEntered = false;
+        private bool MousePressed = false;
+        public Button()
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw | ControlStyles.SupportsTransparentBackColor | ControlStyles.UserPaint, true);
+            DoubleBuffered = true;//позбавляємся від мерехтінь
+            Size = new Size(100, 30);//Задаємо початковий розмір кнопки.
+            BackColor = Color.BlueViolet;
+            ForeColor = Color.White;//колір тексту
+            SF.Alignment = StringAlignment.Center;
+            SF.LineAlignment = StringAlignment.Center;//вирівнюємо по виретикалі та горизонталі, щоб текст був всередині кнопки.
+            
+        }
+        protected override void OnPaint(PaintEventArgs e) // Викликається, коли необхідно перемалювати об'єкт.
+        {
+            base.OnPaint(e);//Вкиликаємо базовий метод.
+            Graphics graph = e.Graphics;// Об'являємо об'кт класу Graphics, з його допомогою виконується вся візуалізація графічного інтерфейсу.
+            graph.SmoothingMode = SmoothingMode.HighQuality;//Вкахуємо високу якість візуалізації.
+            graph.Clear(Parent.BackColor);//Викликаємо метод Clear, даний метод очищує всю поверхню малювання і заливає її вказаним кольором, в даному випадку BackColor батьківського контейнера
+            Rectangle rect = new Rectangle(0, 0, Width - 1, Height - 1);// Зменшили розмір прямокутника по ширині та висоті на один піксель,тому що малювалися б лише два ребра(ліве та верхнє), так сталолося тому, що інші два ребра проходять на границі видимій області кнопки.
+            graph.DrawRectangle(new Pen(BackColor), rect);
+            graph.FillRectangle(new SolidBrush(BackColor), rect);//заиваємо нашу кнопку.
+            if (MouseEntered)//Якщо миша наведена на кнопку, то малюємо зверху ще один прямокутник поверх основного.
+            {
+                graph.DrawRectangle(new Pen(Color.FromArgb(60, Color.White)), rect);
+                graph.FillRectangle(new SolidBrush(Color.FromArgb(60, Color.White)), rect);
+            }
+            if (MousePressed)//Якщо натискаємо на кнопку, то малюємо зверху ще один прямокутник поверх основного.
+            {
+                graph.DrawRectangle(new Pen(Color.FromArgb(30, Color.Black)), rect);
+                graph.FillRectangle(new SolidBrush(Color.FromArgb(30, Color.Black)), rect);
+            }
+            graph.DrawString(Text, Font, new SolidBrush(ForeColor), rect, SF);
+        }
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            MouseEntered = true;
+            Invalidate();//Виклакаємо Invalidate, щоб метод OnPaint викликався кожен раз при наведені миші, таким чином буде виконуватись перемальовування кнопки з урахуванням змінених параметрів.
+        }
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            MouseEntered = false;
+            Invalidate();
+        }
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            MousePressed = true;
+            Invalidate();
+        }
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            MousePressed = false;
+            Invalidate();
+        }
+    }
+}
---
+++ Component/Component/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Component
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new Component.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.BlueViolet;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(285, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 97);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Кнопка";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
+
